4a4be781b5e9063f2bb0ab4b88fcf73a34744a88
FixedFish <>
2025-07-13 16:00:19 +0500

    Add code autocompletion

--- init.lua
+++ init.lua
@@ -1,15 +1,11 @@
+require('core.lazy')
 require('core.lsp')
 vim.o.number = true
 vim.o.rnu = true
 vim.o.breakindent = true
 vim.o.mouse = ''
 
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
-
-vim.schedule(function() 
-	vim.o.clipboard='unnamedplus'
-end)
+vim.o.clipboard='unnamedplus'
 
 vim.cmd('colo retrobox')
 
